67b22739c9b7c0f220cbc2f5f5600750d3d89906
Debian <bitnami@ip-172-26-14-6.us-east-2.compute.internal>
2021-11-28 11:58:44 +0000

    Parse the post parameters

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+const process  = require('process')
 const http     = require('http')
-const https    = require('https')
 const express  = require('express')
 const app      = express()
-const port     = 8888
-const hostname = 'node-whatsapp.thenshow.me'
 
-app.get('/messageIn', (req, res) => {
-    console.log(`uh, cant remember: body first? ${req.body.To} or nah? ${req.To}`)
-    console.log(`uh, cant remember: body first? ${req.body.From} or nah? ${req.From}`)
-    console.log(`uh, cant remember: body first? ${req.body.Body} or nah? ${req.Body}`)
+var myArgs     = process.argv.slice(2);
+const port     = myArgs.includes('-p') ? myArgs[myArgs.indexOf('-p')+1] : 8888
+//const hostname = 'node-whatsapp.thenshow.me'
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.post('/messageIn', (req, res) => {
+    const body = req.body;
+    console.log(" To " + body.To + " ** From " + body.From + " ** Body " + body.Body)
     res.send('Hello World!')
 })
 
-app.get('/statusCallback', (req, res) => {
+app.post('/statusCallback', (req, res) => {
     res.send('No appropriate response forthcoming :/')
 })
 
-http.createServer(app).listen(port, hostname, 511, () => {
-    console.log(`Example app listening at http://${hostname}:${port}`)
+app.listen(port, () => {
+    console.log(`Example app listening at http://hereye:${port}`)
 });
 
 
